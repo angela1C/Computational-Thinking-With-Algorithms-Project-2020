453df8f563e07433034392711630e25edee3def0
angela1C <fange21@icloud.com>
2020-04-29 15:03:09 +0100

    python code for insertion sort algorithm

---
+++ insertion.py
@@ -0,0 +1,34 @@
+"""
+Insertion Sort algorithm
+
+Code adapted and commented based on java code in lecture notes and python code at  https://runestone.academy/runestone/books/published/pythonds/SortSearch/TheInsertionSort.html
+and geekforgeeks.com
+"""
+
+
+# my adapted and commented version based on lecture notes and codes at interactive python, geeks etc.
+def insertionSort(array):
+    # starting at the 2nd element in the list of item, at index 1
+    for i in range(1,len(array)):
+    # iterate through the array, each time setting the key to be the next element in the array
+    # the key is the item to be positioned  
+        key = array[i]
+    # initialise j, j to be used to find the correct position of the key element, looks to element eft of the current key
+        j = i -1
+    # while key element is smaller than elements to it's left, move all elements greater than the key right by one position
+        while j >= 0 and array[j] > key:
+            array[j+1] = array[j]
+         # reposition j to point to the next element
+            j -= 1
+
+    # after shifting elements, move key to its correct new position after the element just smaller than it.
+        array[j+1]=key
+
+import random
+x = [random.randint(0,30) for i in range(20)]
+
+if __name__ == "__main__":
+
+    print(f"The array before being sorted using insertion sort \n {x}")
+    insertionSort(x)
+    print(f" The array after being sorted \n {x}")
